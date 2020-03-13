5645f1fa79a1f26d73ac0351b03672d10219f19a
b21606478 <b21606478@cs.hacettepe.edu.tr>
2020-03-13 20:46:41 +0300

    AudioBooks, ElectronicBook, and PhysicalBook models are added.

--- src/main/java/com/oak/bookyourshelf/model/ElectronicBook.java
+++ src/main/java/com/oak/bookyourshelf/model/ElectronicBook.java
@@ -1,4 +1,26 @@
 package com.oak.bookyourshelf.model;
 
 public class ElectronicBook extends Book {
+
+    private int pageNum;
+    private int fileSize;
+    enum FileFormat { EPUB , PDF; }
+
+    public int getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(int fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public int getPageNum() {
+        return pageNum;
+    }
+
+    public void setPageNum(int pageNum) {
+        this.pageNum = pageNum;
+    }
+
+
 }
--- src/main/java/com/oak/bookyourshelf/model/PhysicalBook.java
+++ src/main/java/com/oak/bookyourshelf/model/PhysicalBook.java
@@ -1,4 +1,45 @@
 package com.oak.bookyourshelf.model;
 
 public class PhysicalBook extends Book {
+
+    private int pageNum;
+    private int editionNum;
+    private float height;
+    private float width;
+    enum MediaType { HARDCOVER, SOFTCOVER, POCKET_SIZE, TABLET_SIZE; }
+    enum PaperType { COATED, UN_COATED, AMERICAN_BRISTOL, TRACING, CANVAS, CELLOPHANE, CRAFT, MATTE_COATED_PLASTER; }
+    enum BindingType { SADDLE_STITCHING , CASE_BINDING, CASE_WRAPPED, PLASTIC_COIL, PERFECT_BINDING; }
+    
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public void setWidth(float width) {
+        this.width = width;
+    }
+
+    public int getEditionNum() {
+        return editionNum;
+    }
+
+    public void setEditionNum(int editionNum) {
+        this.editionNum = editionNum;
+    }
+
+    public int getPageNum() {
+        return pageNum;
+    }
+
+    public void setPageNum(int pageNum) {
+        this.pageNum = pageNum;
+    }
+
 }
