a144593da0ee1921081a3b69e375909f20f184ca
oguzbakir <b21627007@cs.hacettepe.edu.tr>
2020-04-10 00:12:00 +0300

    Add Creditcard model

---
+++ src/main/java/com/oak/bookyourshelf/model/CreditCard.java
@@ -0,0 +1,53 @@
+package com.oak.bookyourshelf.model;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+
+public class CreditCard implements Serializable {
+    String fullName;
+    String cardNumber;
+    String expMonth;
+    String expYear;
+
+    int cvv;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getExpMonth() {
+        return expMonth;
+    }
+
+    public void setExpMonth(String expMonth) {
+        this.expMonth = expMonth;
+    }
+
+    public String getExpYear() {
+        return expYear;
+    }
+
+    public void setExpYear(String expYear) {
+        this.expYear = expYear;
+    }
+
+    public int getCvv() {
+        return cvv;
+    }
+
+    public void setCvv(int cvv) {
+        this.cvv = cvv;
+    }
+}
