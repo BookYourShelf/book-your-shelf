5879efc1bfc1d3e3cae89c91b0d2c02242236e45
fidansamet <fidansamet@hacettepe.edu.tr>
2020-04-14 13:57:37 +0300

    Campaign controller created

---
+++ src/main/java/com/oak/bookyourshelf/controller/campaign_details/CampaignDetailsController.java
@@ -0,0 +1,15 @@
+package com.oak.bookyourshelf.controller.campaign_details;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CampaignDetailsController {
+
+    @RequestMapping("/campaign-details")
+    public String showAdminPanelPage(Model model) {
+
+        return "campaign-details";
+    }
+}
