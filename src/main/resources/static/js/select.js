6b1f39ab58860ff292e724b54612e7c193906bd5
oguzbakir <b21627007@cs.hacettepe.edu.tr>
2020-04-14 13:30:49 +0300

    Fix hashchange navigation

--- src/main/resources/static/js/select.js
+++ src/main/resources/static/js/select.js
@@ -23,3 +23,15 @@ $('#siteTab a').click(function (e) {
         pane.tab('show');
     });
 });
+
+var tabs$ = $(".nav-tabs a");
+
+$(window).on("hashchange", function () {
+    var hash = window.location.hash, // get current hash
+        menu_item$ = tabs$.filter('[href="' + hash + '"]'); // get the menu element
+
+    var url = menu_item$.attr("data-url");
+    $(hash).load(url, function (result) {
+        menu_item$.tab("show"); // call bootstrap to show the tab
+    });
+}).trigger("hashchange");
