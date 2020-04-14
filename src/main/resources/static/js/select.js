37c60987d11a759c406b7672ddadd43b3ecf5668
oguzbakir <b21627007@cs.hacettepe.edu.tr>
2020-04-14 10:17:13 +0300

    Split up admin-panel.html tabs for controllers

--- src/main/resources/static/js/select.js
+++ src/main/resources/static/js/select.js
@@ -12,3 +12,19 @@ $('select').each(
         });
     }
 );
+$('#myTab a').click(function (e) {
+    e.preventDefault();
+
+    var url = $(this).attr("data-url");
+    var href = this.hash;
+    var pane = $(this);
+
+    // ajax load from data-url
+    $(href).load(url, function (result) {
+        pane.tab('show');
+    });
+});
+
+// load first tab content
+$('#user').load($('.nav-link.active').attr("data-url"), function (result) {
+});
