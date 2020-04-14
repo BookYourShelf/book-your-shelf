fec371b9a1386dda032c778097e9c99fe6742e44
oguzbakir <b21627007@cs.hacettepe.edu.tr>
2020-04-14 11:13:57 +0300

    Rename product function in AdminPanelProductController.java and remove unnecessary model variable

--- src/main/java/com/oak/bookyourshelf/controller/admin_panel/AdminPanelProductController.java
+++ src/main/java/com/oak/bookyourshelf/controller/admin_panel/AdminPanelProductController.java
@@ -1,13 +1,12 @@
 package com.oak.bookyourshelf.controller.admin_panel;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminPanelProductController {
     @GetMapping("/admin-panel/product")
-    public String product(Model model) {
+    public String tab() {
 
         return "admin_panel/_product";
     }
