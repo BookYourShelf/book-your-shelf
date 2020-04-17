e20426894ece69f785a946c04294bce9cb4dc05e
oguzbakir <b21627007@cs.hacettepe.edu.tr>
2020-04-17 13:54:36 +0300

    Save categories to database

--- src/main/java/com/oak/bookyourshelf/controller/admin_panel/AdminPanelCategoryController.java
+++ src/main/java/com/oak/bookyourshelf/controller/admin_panel/AdminPanelCategoryController.java
@@ -1,15 +1,22 @@
 package com.oak.bookyourshelf.controller.admin_panel;
 
 import com.oak.bookyourshelf.model.Category;
+import com.oak.bookyourshelf.service.admin_panel.AdminPanelCategoryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @Controller
 public class AdminPanelCategoryController {
+    final
+    AdminPanelCategoryService adminPanelCategoryService;
+
+    public AdminPanelCategoryController(AdminPanelCategoryService adminPanelCategoryService) {
+        this.adminPanelCategoryService = adminPanelCategoryService;
+    }
 
     @RequestMapping(value = "/admin-panel/category", method = RequestMethod.GET)
     public String tab(Model model) {
@@ -20,9 +27,10 @@ public class AdminPanelCategoryController {
     }
 
     @RequestMapping(value = "/admin-panel/category", method = RequestMethod.POST)
-    public String saveCategory(@Valid @ModelAttribute Category category, Model model) {
-        if (!StringUtils.isEmpty(category.getName())) {
-
+    public String saveCategory(@Valid @ModelAttribute Category category, BindingResult error, Model model) {
+        if (!error.hasErrors()) {
+            adminPanelCategoryService.save(category);
+            return "redirect:/admin-panel#category";
         }
         System.out.println("hede");
         return "redirect:/admin-panel#category";
--- src/main/java/com/oak/bookyourshelf/service/admin_panel/AdminPanelCategoryService.java
+++ src/main/java/com/oak/bookyourshelf/service/admin_panel/AdminPanelCategoryService.java
@@ -1,5 +1,7 @@
 package com.oak.bookyourshelf.service.admin_panel;
 
+import com.oak.bookyourshelf.model.Category;
+import com.oak.bookyourshelf.model.Payment;
 import com.oak.bookyourshelf.repository.admin_panel.AdminPanelCategoryRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,5 +14,21 @@ public class AdminPanelCategoryService {
 
     public AdminPanelCategoryService(AdminPanelCategoryRepository adminPanelCategoryRepository) {
         this.adminPanelCategoryRepository = adminPanelCategoryRepository;
+    }
+
+    public Iterable<Category> listAll() {
+        return adminPanelCategoryRepository.findAll();
+    }
+
+    public void save(Category category) {
+        adminPanelCategoryRepository.save(category);
+    }
+
+    public Category get(int id) {
+        return adminPanelCategoryRepository.findById(id).get();
+    }
+
+    public void delete(int id) {
+        adminPanelCategoryRepository.deleteById(id);
     }
 }
