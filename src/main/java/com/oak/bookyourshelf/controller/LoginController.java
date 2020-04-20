2d0fc3d2b2767a62e63b8ba0856dcd7b34bff3ea
b21606478 <b21606478@cs.hacettepe.edu.tr>
2020-04-20 11:21:29 +0300

    Validation of user email address and password performed.

---
+++ src/main/java/com/oak/bookyourshelf/controller/LoginController.java
@@ -0,0 +1,81 @@
+package com.oak.bookyourshelf.controller;
+
+
+import com.oak.bookyourshelf.model.User;
+import com.oak.bookyourshelf.service.LoginService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Base64;
+
+@Controller
+public class LoginController {
+
+    final
+    LoginService loginService;
+    private static byte[] key;
+    private static SecretKeySpec secretKey;
+
+    public LoginController(LoginService loginService) {
+        this.loginService = loginService;
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String showLoginPage(Model model) {
+        return "login";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity<String> loginUser(@RequestParam String email, User user) throws NoSuchAlgorithmException, BadPaddingException, IllegalBlockSizeException, NoSuchPaddingException, InvalidKeyException, UnsupportedEncodingException {
+        if (email != null) {
+            User existing = loginService.findByEmail(email);
+            if (existing == null) {
+                return ResponseEntity.badRequest().body("User is not registered.Please create an account to login.");
+            } else {
+                prepareSecreteKey("secrete");
+                Cipher cipher = Cipher.getInstance("AES");
+                cipher.init(Cipher.DECRYPT_MODE, secretKey);
+                String newPassword = new String(cipher.doFinal(Base64.getDecoder().decode(existing.getPassword())));
+
+
+                if (!(user.getPassword().equals(newPassword))) {
+                    return ResponseEntity.badRequest().body("Entered password is not correct.Please enter the correct password.");
+                }
+            }
+        }
+
+        return ResponseEntity.ok("");
+    }
+
+    public void prepareSecreteKey(String myKey) {
+        MessageDigest sha = null;
+        try {
+            key = myKey.getBytes(StandardCharsets.UTF_8);
+            key = MessageDigest.getInstance("SHA-1").digest(key);
+            key = Arrays.copyOf(key, 16);
+            secretKey = new SecretKeySpec(key, "AES");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
