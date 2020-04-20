b0a92e8e4a3a36cd0ca332217ab0e05f803c2449
b21606478 <b21606478@cs.hacettepe.edu.tr>
2020-04-20 11:16:38 +0300

    Password encrypted and saved to the database.

--- src/main/java/com/oak/bookyourshelf/controller/RegisterController.java
+++ src/main/java/com/oak/bookyourshelf/controller/RegisterController.java
@@ -13,7 +13,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
 import javax.validation.Valid;
+import javax.xml.bind.DatatypeConverter;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Base64;
 
 
 @Controller
@@ -21,6 +34,8 @@ public class RegisterController {
 
     final
     RegisterService registerService;
+    private static byte[] key;
+    private static SecretKeySpec secretKey;
 
     public RegisterController(RegisterService registerService) {
         this.registerService = registerService;
@@ -36,14 +51,35 @@ public class RegisterController {
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
     @ResponseBody
-    public ResponseEntity<String> registerUser(@RequestParam String email, User user) {
+    public ResponseEntity<String> registerUser(@RequestParam String email, User user) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, UnsupportedEncodingException, BadPaddingException, IllegalBlockSizeException {
         if (email != null) {
             User existing = registerService.findByEmail(email);
             if (existing != null) {
                 return ResponseEntity.badRequest().body("This email address already exists. Please try another one.");
             }
         }
+
+        prepareSecreteKey("secrete");
+        Cipher cipher =Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE,secretKey);
+        String newPassword= Base64.getEncoder().encodeToString(cipher.doFinal(user.getPassword().getBytes("UTF-8")));
+
+        user.setPassword(newPassword);
+
+
         registerService.save(user);
         return ResponseEntity.ok("");
+    }
+
+    public void prepareSecreteKey(String myKey) {
+        MessageDigest sha = null;
+        try {
+            key = myKey.getBytes(StandardCharsets.UTF_8);
+            key = MessageDigest.getInstance("SHA-1").digest(key);
+            key = Arrays.copyOf(key, 16);
+            secretKey = new SecretKeySpec(key, "AES");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
     }
 }
