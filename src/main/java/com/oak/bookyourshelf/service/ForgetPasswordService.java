4d7c30e740430e424cfa10c5ac49e616807bb6f0
b21606478 <b21606478@cs.hacettepe.edu.tr>
2020-04-24 12:03:48 +0300

    Forget Password service added.

---
+++ src/main/java/com/oak/bookyourshelf/service/ForgetPasswordService.java
@@ -0,0 +1,29 @@
+package com.oak.bookyourshelf.service;
+
+
+import com.oak.bookyourshelf.model.Log;
+import com.oak.bookyourshelf.model.User;
+import com.oak.bookyourshelf.repository.ForgetPasswordRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class ForgetPasswordService {
+
+    final
+    ForgetPasswordRepository forgetPasswordRepository;
+
+    public ForgetPasswordService(ForgetPasswordRepository forgetPasswordRepository) {
+        this.forgetPasswordRepository = forgetPasswordRepository;
+    }
+
+    public User findByEmail(String email) {
+        return forgetPasswordRepository.findByEmail(email);
+    }
+
+    public void save(Log log) {
+        forgetPasswordRepository.save(log);
+    }
+
+}
