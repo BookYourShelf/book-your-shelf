b40c2494bd0a1d7c596466cba8ab0f197719b82d
b21606478 <b21606478@cs.hacettepe.edu.tr>
2020-04-24 12:08:32 +0300

    EXistance and expiration of token checked and then user change password functionality added.

---
+++ src/main/java/com/oak/bookyourshelf/controller/NewPasswordController.java
@@ -0,0 +1,78 @@
+package com.oak.bookyourshelf.controller;
+
+import com.oak.bookyourshelf.model.Log;
+import com.oak.bookyourshelf.model.PasswordChange;
+import com.oak.bookyourshelf.model.User;
+import com.oak.bookyourshelf.service.LoginService;
+import com.oak.bookyourshelf.service.NewPasswordService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.sql.Timestamp;
+
+
+@Controller
+public class NewPasswordController {
+
+    final
+    NewPasswordService newPasswordService;
+    final
+    LoginService loginService;
+    public final PasswordEncoder passwordEncoder;
+    String tokens;
+
+    public NewPasswordController(NewPasswordService newPasswordService, LoginService loginService, PasswordEncoder passwordEncoder) {
+        this.newPasswordService = newPasswordService;
+        this.loginService = loginService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+    @RequestMapping(value = "/new-password", method = RequestMethod.GET)
+    public String showNewPasswordPage(@RequestParam("token") String token, Model model) {
+        Log log = newPasswordService.findByToken(token);
+
+        if (log == null) {
+            model.addAttribute("error", "Couldn't find token");
+            return "forgot-password";
+        } else {
+            /*Link Expiration Check in minutes*/
+            long currInMilliSecond = new Timestamp(System.currentTimeMillis()).getTime();
+            long timeInMilliSecond = log.getTime().getTime();
+            int expire = (int) ((currInMilliSecond - timeInMilliSecond) / (60 * 1000));
+
+            if (expire > 60) {
+                System.out.println("Link has expired.");
+                model.addAttribute("error", "Token has expired");
+                return "forgot-password";
+            } else {
+                model.addAttribute("token", token);
+                tokens = token;
+            }
+        }
+        return "new-password";
+    }
+
+    @RequestMapping(value = "/new-password", method = RequestMethod.POST)
+    public ResponseEntity<String> setNewPassword(@RequestParam String newPassword, @RequestParam String confirmPassword, PasswordChange passwordChange) {
+        Log log = newPasswordService.findByToken(tokens);
+        User user = loginService.findByEmail(log.getEmail());
+
+        if (!newPassword.equals(confirmPassword)) {
+            return ResponseEntity.badRequest().body("Passwords don't match. Please enter your password again.");
+        } else {
+            if (user.getPassword().equals(passwordEncoder.encode(confirmPassword))) {
+                return ResponseEntity.badRequest().body("New Password can't match old Password.Please enter a new password.");
+            }
+            user.setPassword(passwordEncoder.encode(confirmPassword));
+            loginService.save(user);
+        }
+        return ResponseEntity.ok("");
+    }
+
+}
