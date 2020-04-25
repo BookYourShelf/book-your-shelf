b4649c7e0bf1f38fd3cbc91295a4bf736edef60b
fidansamet <fidansamet@hacettepe.edu.tr>
2020-04-26 02:34:39 +0300

    product addition performed

--- src/main/java/com/oak/bookyourshelf/controller/admin_panel/AdminPanelProductController.java
+++ src/main/java/com/oak/bookyourshelf/controller/admin_panel/AdminPanelProductController.java
@@ -1,13 +1,91 @@
 package com.oak.bookyourshelf.controller.admin_panel;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.oak.bookyourshelf.model.*;
+import com.oak.bookyourshelf.service.admin_panel.AdminPanelProductService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 @Controller
 public class AdminPanelProductController {
-    @GetMapping("/admin-panel/product")
-    public String tab() {
+
+    final AdminPanelProductService adminPanelProductService;
+
+    public AdminPanelProductController(AdminPanelProductService adminPanelProductService) {
+        this.adminPanelProductService = adminPanelProductService;
+    }
+
+    @RequestMapping(value = "/admin-panel/product", method = RequestMethod.GET)
+    public String tab(Model model, @ModelAttribute("selectedOption") String selectedOption) {
 
         return "admin_panel/_product";
+    }
+
+    @RequestMapping(value = "/admin-panel/product", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity<String> saveProduct(@RequestParam String productType, PhysicalBook physicalBook,
+                                              ElectronicBook electronicBook, AudioBook audioBook,
+                                              ElectronicBookReader electronicBookReader,
+                                              ElectronicBookReaderCase electronicBookReaderCase,
+                                              PhysicalBookCase physicalBookCase,
+                                              @RequestParam("lists") String lists) throws JsonProcessingException {
+
+        switch (productType) {
+            case "book":
+                return bookBarcodeAndISBNCheck(physicalBook, lists);
+            case "ebook":
+                return bookBarcodeAndISBNCheck(electronicBook, lists);
+            case "audio_book":
+                return bookBarcodeAndISBNCheck(audioBook, lists);
+            case "ebook_reader":
+                return productBarcodeCheck(electronicBookReader);
+            case "ebook_reader_case":
+                return productBarcodeCheck(electronicBookReaderCase);
+            case "book_case":
+                return productBarcodeCheck(physicalBookCase);
+            default:
+                return ResponseEntity.badRequest().body("An error occurred.");
+        }
+    }
+
+    public ResponseEntity<String> productBarcodeCheck(Product product) {
+
+        Product productDb = adminPanelProductService.getByBarcode(product.getBarcode());
+        if (productDb != null) {
+            return ResponseEntity.badRequest().body("Product with given barcode already exists.");
+        }
+
+        adminPanelProductService.save(product);
+        return ResponseEntity.ok("");
+    }
+
+    public ResponseEntity<String> bookBarcodeAndISBNCheck(Book product, String lists) throws JsonProcessingException {
+
+        // Add lists to product
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, ArrayList<String>> map = mapper.readValue(lists, Map.class);
+        product.setPublishers(map.get("publishers"));
+        product.setTranslators(map.get("translators"));
+        product.setAuthors(map.get("authors"));
+        product.setKeywords(map.get("keywords"));
+
+        Product barcodeDb = adminPanelProductService.getByBarcode(product.getBarcode());
+        if (barcodeDb != null) {
+            return ResponseEntity.badRequest().body("Product with given barcode already exists.");
+        }
+
+        Product ISBNDb = adminPanelProductService.getByISBN(product.getIsbn());
+        if (ISBNDb != null) {
+            return ResponseEntity.badRequest().body("Product with given ISBN already exists.");
+        }
+
+        adminPanelProductService.save(product);
+        return ResponseEntity.ok("");
     }
 }
--- src/main/java/com/oak/bookyourshelf/repository/admin_panel/AdminPanelProductRepository.java
+++ src/main/java/com/oak/bookyourshelf/repository/admin_panel/AdminPanelProductRepository.java
@@ -1,4 +1,13 @@
 package com.oak.bookyourshelf.repository.admin_panel;
 
-public class AdminPanelProductRepository {
+import com.oak.bookyourshelf.model.Product;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AdminPanelProductRepository extends CrudRepository<Product, Integer> {
+    @Query("SELECT t FROM Product t WHERE t.barcode = ?1")
+    Product findProductByBarcode(int barcode);
+
+    @Query("SELECT t FROM Book t WHERE t.isbn = ?1")
+    Product findBookByISBN(String isbn);
 }
