b786b0e5a8945ad48b19c04505f26f844b504452
iseridilara <b21783561@cs.hacettepe.edu.tr>
2020-05-05 22:15:26 +0300

    User details search history controller service repository added

--- src/main/java/com/oak/bookyourshelf/controller/user_details/UserDetailsSearchController.java
+++ src/main/java/com/oak/bookyourshelf/controller/user_details/UserDetailsSearchController.java
@@ -1,14 +1,30 @@
 package com.oak.bookyourshelf.controller.user_details;
 
+import com.oak.bookyourshelf.model.User;
+import com.oak.bookyourshelf.service.user_details.UserDetailsSearchService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class UserDetailsSearchController {
 
-    @GetMapping("/user-details/search")
-    public String tab() {
+    final UserDetailsSearchService userDetailsSearchService;
 
-        return "user_details/_search";
+    public UserDetailsSearchController(UserDetailsSearchService userDetailsSearchService) {
+        this.userDetailsSearchService = userDetailsSearchService;
+    }
+
+
+    @RequestMapping(value = "/user-details/search/{id}", method = RequestMethod.GET)
+    public String tab(Model model, @PathVariable("id") int id) {
+
+        User user = userDetailsSearchService.get(id);
+        model.addAttribute("searchValues", user.getSearchHistory());
+
+        return "/user_details/_search";
     }
 }
--- src/main/java/com/oak/bookyourshelf/service/user_details/UserDetailsSearchService.java
+++ src/main/java/com/oak/bookyourshelf/service/user_details/UserDetailsSearchService.java
@@ -1,4 +1,44 @@
 package com.oak.bookyourshelf.service.user_details;
 
+
+import com.oak.bookyourshelf.model.User;
+import com.oak.bookyourshelf.repository.user_details.UserDetailsSearchRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
 public class UserDetailsSearchService {
+
+    final UserDetailsSearchRepository userDetailsSearchRepository;
+
+    public UserDetailsSearchService(UserDetailsSearchRepository userDetailsSearchRepository) {
+        this.userDetailsSearchRepository = userDetailsSearchRepository;
+    }
+
+    public Iterable<User> listAll() {
+        return userDetailsSearchRepository.findAll();
+    }
+
+    public User get(int id) {
+        return userDetailsSearchRepository.findById(id).get();
+    }
+
+    public void delete(int id) {
+        userDetailsSearchRepository.deleteById(id);
+    }
+
+    public void save(User user) {
+        userDetailsSearchRepository.save(user);
+    }
+
+    public User getByEmail(String email) {
+        return userDetailsSearchRepository.findUserByEmail(email);
+    }
+
+    public List<Integer> findAllIds() {
+        return userDetailsSearchRepository.findAllIds();
+    }
 }
