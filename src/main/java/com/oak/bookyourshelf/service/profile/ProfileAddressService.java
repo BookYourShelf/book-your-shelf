2dd766dd25623d1bb0e8979f220340e7f835d5f2
b21606478 <b21606478@cs.hacettepe.edu.tr>
2020-05-12 16:09:30 +0300

    Profile address service added.

--- src/main/java/com/oak/bookyourshelf/service/profile/ProfileAddressService.java
+++ src/main/java/com/oak/bookyourshelf/service/profile/ProfileAddressService.java
@@ -1,4 +1,25 @@
 package com.oak.bookyourshelf.service.profile;
 
+import com.oak.bookyourshelf.model.Address;
+import com.oak.bookyourshelf.repository.profile.ProfileAddressRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
 public class ProfileAddressService {
+    final ProfileAddressRepository profileAddressRepository;
+
+    public ProfileAddressService(ProfileAddressRepository profileAddressRepository){
+        this.profileAddressRepository = profileAddressRepository;
+    }
+
+    public void delete(int id) {
+        profileAddressRepository.deleteById(id);
+    }
+
+    public void save(Address address) {
+        profileAddressRepository.save(address);
+    }
+
 }
