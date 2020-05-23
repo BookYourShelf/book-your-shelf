8218c250c55e771d9da0d6a7c6061726f9c12254
iseridilara <b21783561@cs.hacettepe.edu.tr>
2020-05-23 03:10:21 +0300

    AdminPanelCampaignController was added

--- src/main/java/com/oak/bookyourshelf/controller/admin_panel/AdminPanelCampaignController.java
+++ src/main/java/com/oak/bookyourshelf/controller/admin_panel/AdminPanelCampaignController.java
@@ -1,13 +1,44 @@
 package com.oak.bookyourshelf.controller.admin_panel;
 
+import com.oak.bookyourshelf.model.Campaign;
+import com.oak.bookyourshelf.service.admin_panel.AdminPanelCampaignService;
+import com.oak.bookyourshelf.service.admin_panel.AdminPanelCategoryService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class AdminPanelCampaignController {
-    @GetMapping("/admin-panel/campaign")
-    public String tab() {
+    final AdminPanelCampaignService adminPanelCampaignService;
+    final AdminPanelCategoryService adminPanelCategoryService;
+
+    public AdminPanelCampaignController(AdminPanelCampaignService adminPanelCampaignService, AdminPanelCategoryService adminPanelCategoryService) {
+        this.adminPanelCampaignService = adminPanelCampaignService;
+        this.adminPanelCategoryService = adminPanelCategoryService;
+    }
+
+    @RequestMapping(value = "/admin-panel/campaign", method = RequestMethod.GET)
+    public String tab(Model model) {
+        System.out.println("get method");
+        Campaign campaign = new Campaign();
+        model.addAttribute("campaign", campaign);
+        model.addAttribute("allCampaigns", adminPanelCampaignService.listAll());
+        model.addAttribute("categoryService", adminPanelCategoryService);
 
         return "admin_panel/_campaign";
+    }
+
+    @RequestMapping(value = "/admin-panel/campaign", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity<String> saveCategory(@RequestParam String name, Campaign campaign) {
+        System.out.println("post method");
+      if( adminPanelCampaignService.getByName(name) != null)
+      {
+          return ResponseEntity.badRequest().body("This campaign name exist");
+      }
+
+        adminPanelCampaignService.save(campaign);
+        return ResponseEntity.ok("");
     }
 }
