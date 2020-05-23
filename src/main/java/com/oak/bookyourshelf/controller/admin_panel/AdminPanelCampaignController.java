f5b5a5d1effcdaccdc79afd3e474c3c764179a2f
iseridilara <b21783561@cs.hacettepe.edu.tr>
2020-05-23 03:34:54 +0300

    AdminPanelCampaignController was fixed for pagination

--- src/main/java/com/oak/bookyourshelf/controller/admin_panel/AdminPanelCampaignController.java
+++ src/main/java/com/oak/bookyourshelf/controller/admin_panel/AdminPanelCampaignController.java
@@ -1,12 +1,20 @@
 package com.oak.bookyourshelf.controller.admin_panel;
 
 import com.oak.bookyourshelf.model.Campaign;
+import com.oak.bookyourshelf.model.User;
 import com.oak.bookyourshelf.service.admin_panel.AdminPanelCampaignService;
 import com.oak.bookyourshelf.service.admin_panel.AdminPanelCategoryService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 public class AdminPanelCampaignController {
@@ -19,12 +27,27 @@ public class AdminPanelCampaignController {
     }
 
     @RequestMapping(value = "/admin-panel/campaign", method = RequestMethod.GET)
-    public String tab(Model model) {
+    public String tab(@RequestParam("page") Optional<Integer> page,
+                      @RequestParam("size") Optional<Integer> size, Model model) {
+        int currentPage = page.orElse(1);
+        int pageSize = size.orElse(3);
         System.out.println("get method");
+
+        Page<Campaign> campaignPage = adminPanelCampaignService.findPaginated(PageRequest.of(currentPage - 1, pageSize));
         Campaign campaign = new Campaign();
         model.addAttribute("campaign", campaign);
+        model.addAttribute("campaignPage", campaignPage);
         model.addAttribute("allCampaigns", adminPanelCampaignService.listAll());
         model.addAttribute("categoryService", adminPanelCategoryService);
+
+        int totalPages = campaignPage.getTotalPages();
+        if (totalPages > 0) {
+            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
+                    .boxed()
+                    .collect(Collectors.toList());
+            model.addAttribute("pageNumbers", pageNumbers);
+        }
+
 
         return "admin_panel/_campaign";
     }
