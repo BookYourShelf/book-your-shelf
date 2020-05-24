234424cdc76d3619ccb9d00bb0b3ae33ca277805
iseridilara <b21783561@cs.hacettepe.edu.tr>
2020-05-24 12:38:04 +0300

    pagination method added

--- src/main/java/com/oak/bookyourshelf/service/admin_panel/AdminPanelHotListService.java
+++ src/main/java/com/oak/bookyourshelf/service/admin_panel/AdminPanelHotListService.java
@@ -1,11 +1,15 @@
 package com.oak.bookyourshelf.service.admin_panel;
 
-
 import com.oak.bookyourshelf.model.HotList;
 import com.oak.bookyourshelf.repository.admin_panel.AdminPanelHotListRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -39,5 +43,25 @@ public class AdminPanelHotListService {
 
     public void delete(int id) {
         adminPanelHotListRepository.deleteById(id);
+    }
+
+    public Page<HotList> findPaginated(Pageable pageable) {
+        List hotlists = (List)adminPanelHotListRepository.findAll();
+        int pageSize = pageable.getPageSize();
+        int currentPage = pageable.getPageNumber();
+        int startItem = currentPage * pageSize;
+        List<HotList> list;
+
+        if (hotlists.size() < startItem) {
+            list = Collections.emptyList();
+        } else {
+            int toIndex = Math.min(startItem + pageSize, hotlists.size());
+            list = hotlists.subList(startItem, toIndex);
+        }
+
+        Page<HotList> hotListPage
+                = new PageImpl<HotList>(list, PageRequest.of(currentPage, pageSize), hotlists.size());
+
+        return hotListPage;
     }
 }
