a5d08707698c034e3f34130d3ee046f03e66ec4d
iseridilara <b21783561@cs.hacettepe.edu.tr>
2020-05-24 12:31:55 +0300

    HotListDetails controller created

--- src/main/java/com/oak/bookyourshelf/controller/HotListDetailsController.java
+++ src/main/java/com/oak/bookyourshelf/controller/HotListDetailsController.java
@@ -1,15 +1,29 @@
 package com.oak.bookyourshelf.controller;
 
+import com.oak.bookyourshelf.model.HotList;
+import com.oak.bookyourshelf.service.HotListDetailsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HotListDetailsController {
 
-    @RequestMapping("/hot-list-details")
-    public String showAdminPanelPage(Model model) {
+    final HotListDetailsService hotListDetailsService;
 
-        return "hot-list-details";
+    public HotListDetailsController(HotListDetailsService hotListDetailsService) {
+        this.hotListDetailsService = hotListDetailsService;
+    }
+
+    @RequestMapping("/hotList-details/{id}")
+    public String showHotListDetailsPage(@PathVariable int id, Model model) {
+        HotList hotList = hotListDetailsService.get(id);
+        if (hotList == null) {
+            return "redirect:/admin-panel";
+        }
+        model.addAttribute("hotList", hotList);
+
+        return "hotList-details";
     }
 }
