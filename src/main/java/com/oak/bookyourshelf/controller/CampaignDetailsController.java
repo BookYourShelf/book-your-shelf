b5a93d26a896a1542e0e57282db2d0d6c646f7d2
iseridilara <b21783561@cs.hacettepe.edu.tr>
2020-05-24 12:31:17 +0300

    CampaignDetails controller created

--- src/main/java/com/oak/bookyourshelf/controller/CampaignDetailsController.java
+++ src/main/java/com/oak/bookyourshelf/controller/CampaignDetailsController.java
@@ -1,14 +1,28 @@
 package com.oak.bookyourshelf.controller;
 
+import com.oak.bookyourshelf.model.Campaign;
+import com.oak.bookyourshelf.service.CampaignDetailsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CampaignDetailsController {
 
-    @RequestMapping("/campaign-details")
-    public String showAdminPanelPage(Model model) {
+    final CampaignDetailsService campaignDetailsService;
+
+    public CampaignDetailsController(CampaignDetailsService campaignDetailsService) {
+        this.campaignDetailsService = campaignDetailsService;
+    }
+
+    @RequestMapping("/campaign-details/{id}")
+    public String showCampaignDetailsPage(@PathVariable int id, Model model) {
+        Campaign campaign = campaignDetailsService.get(id);
+        if (campaign == null) {
+            return "redirect:/admin-panel";
+        }
+        model.addAttribute("campaign", campaign);
 
         return "campaign-details";
     }
