08eeffbfe2eb48416a02962810b829c33baf80d5
iseridilara <b21783561@cs.hacettepe.edu.tr>
2020-05-25 19:30:35 +0300

    CampaignDetailsController was added

--- src/main/java/com/oak/bookyourshelf/controller/CampaignDetailsController.java
+++ src/main/java/com/oak/bookyourshelf/controller/CampaignDetailsController.java
@@ -1,19 +1,23 @@
 package com.oak.bookyourshelf.controller;
 
 import com.oak.bookyourshelf.model.Campaign;
+import com.oak.bookyourshelf.model.HotList;
 import com.oak.bookyourshelf.service.CampaignDetailsService;
+import com.oak.bookyourshelf.service.admin_panel.AdminPanelCategoryService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class CampaignDetailsController {
 
     final CampaignDetailsService campaignDetailsService;
+    final AdminPanelCategoryService adminPanelCategoryService;
 
-    public CampaignDetailsController(CampaignDetailsService campaignDetailsService) {
+    public CampaignDetailsController(CampaignDetailsService campaignDetailsService, AdminPanelCategoryService adminPanelCategoryService) {
         this.campaignDetailsService = campaignDetailsService;
+        this.adminPanelCategoryService = adminPanelCategoryService;
     }
 
     @RequestMapping("/campaign-details/{id}")
@@ -23,7 +27,32 @@ public class CampaignDetailsController {
             return "redirect:/admin-panel";
         }
         model.addAttribute("campaign", campaign);
-
+        model.addAttribute("categoryService", adminPanelCategoryService);
         return "campaign-details";
+    }
+
+    @RequestMapping(value = "/campaign-details/{id}", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity<String> updateCampaign(@PathVariable int id, @RequestParam String button, Campaign newCampaign)  {
+
+        Campaign campaign = campaignDetailsService.get(id);
+        System.out.println("update");
+        switch (button) {
+            case "update_campaign":
+                campaign.setCategories(newCampaign.getCategories());
+                campaign.setRate(newCampaign.getRate());
+                campaign.setEndDate(newCampaign.getEndDate());
+                campaign.setName(newCampaign.getName());
+                campaign.setStartDate(newCampaign.getStartDate());
+                campaign.setProductType(newCampaign.getProductType());
+
+                campaign.setId(id);
+                campaignDetailsService.save(campaign);
+                return ResponseEntity.ok("");
+            case "delete_hotList":
+                campaignDetailsService.delete(id);
+                return ResponseEntity.ok("");
+        }
+        return ResponseEntity.badRequest().body("An error occurred.");
     }
 }
