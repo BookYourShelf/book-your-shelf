cb90823ca8c60cac1a2e3e05d9e6d219beafba75
fidansamet <fidansamet@hacettepe.edu.tr>
2020-05-27 22:15:15 +0300

    pagination method call added to admin panel product controller

--- src/main/java/com/oak/bookyourshelf/controller/admin_panel/AdminPanelProductController.java
+++ src/main/java/com/oak/bookyourshelf/controller/admin_panel/AdminPanelProductController.java
@@ -2,11 +2,10 @@ package com.oak.bookyourshelf.controller.admin_panel;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.oak.bookyourshelf.Globals;
 import com.oak.bookyourshelf.model.*;
 import com.oak.bookyourshelf.service.admin_panel.AdminPanelCategoryService;
 import com.oak.bookyourshelf.service.admin_panel.AdminPanelProductService;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,8 +15,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 @Controller
 public class AdminPanelProductController {
@@ -33,18 +30,8 @@ public class AdminPanelProductController {
     @RequestMapping(value = "/admin-panel/product", method = RequestMethod.GET)
     public String tab(@RequestParam("page") Optional<Integer> page,
                       @RequestParam("size") Optional<Integer> size, Model model) {
-        int currentPage = page.orElse(1);
-        int pageSize = size.orElse(3);
-        Page<Product> productPage = adminPanelProductService.findPaginated(PageRequest.of(currentPage - 1, pageSize));
-        int totalPages = productPage.getTotalPages();
 
-        if (totalPages > 0) {
-            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages).boxed().collect(Collectors.toList());
-            model.addAttribute("pageNumbers", pageNumbers);
-        }
-
-        model.addAttribute("currentPage", currentPage);
-        model.addAttribute("productPage", productPage);
+        Globals.getPageNumbers(page, size, (List) adminPanelProductService.listAll(), model, "productPage");
         model.addAttribute("allProducts", adminPanelProductService.listAll());
         model.addAttribute("categoryService", adminPanelCategoryService);
         return "admin_panel/_product";
