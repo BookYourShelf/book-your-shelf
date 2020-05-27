a165b682a59c9ac26d26901b9eff270addeec140
iseridilara <b21783561@cs.hacettepe.edu.tr>
2020-05-28 00:30:43 +0300

    calling pagination method changed , globals class was used in AdminPanelCampaignController

--- src/main/java/com/oak/bookyourshelf/controller/admin_panel/AdminPanelCampaignController.java
+++ src/main/java/com/oak/bookyourshelf/controller/admin_panel/AdminPanelCampaignController.java
@@ -1,6 +1,8 @@
 package com.oak.bookyourshelf.controller.admin_panel;
 
+import com.oak.bookyourshelf.Globals;
 import com.oak.bookyourshelf.model.Campaign;
+import com.oak.bookyourshelf.model.Category;
 import com.oak.bookyourshelf.model.User;
 import com.oak.bookyourshelf.service.admin_panel.AdminPanelCampaignService;
 import com.oak.bookyourshelf.service.admin_panel.AdminPanelCategoryService;
@@ -11,9 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -30,44 +30,53 @@ public class AdminPanelCampaignController {
     @RequestMapping(value = "/admin-panel/campaign", method = RequestMethod.GET)
     public String tab(@RequestParam("page") Optional<Integer> page,
                       @RequestParam("size") Optional<Integer> size, Model model) {
-        int currentPage = page.orElse(1);
-        int pageSize = size.orElse(3);
-        System.out.println("get method");
 
-        Page<Campaign> campaignPage = adminPanelCampaignService.findPaginated(PageRequest.of(currentPage - 1, pageSize));
+        Globals.getPageNumbers(page, size, (List) adminPanelCampaignService.listAll(), model, "campaignPage");
+
         Campaign campaign = new Campaign();
         model.addAttribute("campaign", campaign);
-        model.addAttribute("campaignPage", campaignPage);
         model.addAttribute("allCampaigns", adminPanelCampaignService.listAll());
         model.addAttribute("categoryService", adminPanelCategoryService);
-        model.addAttribute("currentPage", currentPage);
-        int totalPages = campaignPage.getTotalPages();
-        if (totalPages > 0) {
-            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
-                    .boxed()
-                    .collect(Collectors.toList());
-            model.addAttribute("pageNumbers", pageNumbers);
-        }
-
 
         return "admin_panel/_campaign";
     }
 
     @RequestMapping(value = "/admin-panel/campaign", method = RequestMethod.POST)
     @ResponseBody
-    public ResponseEntity<String> saveCategory(@RequestParam String name , String ctg, Campaign campaign) {
+    public ResponseEntity<String> saveCategory(@RequestParam String name, String ctg, Campaign campaign) {
         System.out.println("post method");
         System.out.println(ctg);
         String[] category = ctg.split("-");
-        System.out.println(category[0]);
-        System.out.println(category[1]);
-        System.out.println(category.length);
-      if( adminPanelCampaignService.getByName(name) != null)
-      {
-          return ResponseEntity.badRequest().body("This campaign name exist");
-      }
+        List<String> categories = Arrays.asList(category);
+        Map<String, Boolean> campaignCheck = new HashMap<String, Boolean>();
+        for (String m : categories) {
+            campaignCheck.put(m, true);
+        }
 
-        adminPanelCampaignService.save(campaign);
+        List<Campaign> sameType = adminPanelCampaignService.findAllByProductType(campaign.getProductType());
+        for (Campaign i : sameType) {
+            for (String s : categories) {
+                if (i.getCategories().contains(adminPanelCategoryService.getByName(s))) {
+                    campaignCheck.replace(s, false);
+                }
+            }
+        }
+        int categoryNum = 0;
+        List<Category> newCategories = new ArrayList<Category>();
+        for (String s : campaignCheck.keySet()) {
+            if (campaignCheck.get(s)) {
+                Category c = adminPanelCategoryService.getByName(s);
+                newCategories.add(c);
+                categoryNum++;
+            }
+        }
+        if (categoryNum == 0)
+            return ResponseEntity.badRequest().body("There is a campaign in your all select category of selected product type");
+        else {
+            campaign.setCategories(newCategories);
+            adminPanelCampaignService.save(campaign);
+        }
+
         return ResponseEntity.ok("");
     }
 }
