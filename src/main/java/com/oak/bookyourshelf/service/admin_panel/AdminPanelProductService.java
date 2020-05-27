419f2f6c66919a4d265990795852cbc4fefcb34f
fidansamet <fidansamet@hacettepe.edu.tr>
2020-05-27 22:17:43 +0300

    pagination method removed from admin panel product service

--- src/main/java/com/oak/bookyourshelf/service/admin_panel/AdminPanelProductService.java
+++ src/main/java/com/oak/bookyourshelf/service/admin_panel/AdminPanelProductService.java
@@ -2,19 +2,12 @@ package com.oak.bookyourshelf.service.admin_panel;
 
 import com.oak.bookyourshelf.model.Product;
 import com.oak.bookyourshelf.repository.admin_panel.AdminPanelProductRepository;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.util.Collections;
-import java.util.List;
 
 @Service
 public class AdminPanelProductService {
 
-    final AdminPanelProductRepository adminPanelProductRepository;
+    public final AdminPanelProductRepository adminPanelProductRepository;
 
     public AdminPanelProductService(AdminPanelProductRepository adminPanelProductRepository) {
         this.adminPanelProductRepository = adminPanelProductRepository;
@@ -34,25 +27,5 @@ public class AdminPanelProductService {
 
     public Product getByISBN(String isbn) {
         return adminPanelProductRepository.findBookByISBN(isbn);
-    }
-
-    public Page<Product> findPaginated(Pageable pageable) {
-        List products = (List) adminPanelProductRepository.findAllProducts();
-        int pageSize = pageable.getPageSize();
-        int currentPage = pageable.getPageNumber();
-        int startItem = currentPage * pageSize;
-        List<Product> list;
-
-        if (products.size() < startItem) {
-            list = Collections.emptyList();
-        } else {
-            int toIndex = Math.min(startItem + pageSize, products.size());
-            list = products.subList(startItem, toIndex);
-        }
-
-        Page<Product> productPage
-                = new PageImpl<Product>(list, PageRequest.of(currentPage, pageSize), products.size());
-
-        return productPage;
     }
 }
