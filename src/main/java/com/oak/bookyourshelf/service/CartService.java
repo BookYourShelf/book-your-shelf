ed69c7f0cae716083a37b7e090f4a55c5f72532c
b21606478 <b21606478@cs.hacettepe.edu.tr>
2020-05-30 23:25:07 +0300

    Cart service added

--- src/main/java/com/oak/bookyourshelf/service/CartService.java
+++ src/main/java/com/oak/bookyourshelf/service/CartService.java
@@ -1,7 +1,18 @@
 package com.oak.bookyourshelf.service;
 
+import com.oak.bookyourshelf.model.Product;
+import com.oak.bookyourshelf.repository.CartRepository;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
+@Transactional
 public class CartService {
+    final CartRepository cartRepository;
+
+    public CartService(CartRepository cartRepository){ this.cartRepository= cartRepository; }
+
+
+
 }
