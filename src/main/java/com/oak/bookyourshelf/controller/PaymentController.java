aafd7541945528b16dbf68b32f5458b8249523e2
b21606478 <b21606478@cs.hacettepe.edu.tr>
2020-05-31 15:29:24 +0300

    Order payment processed and product attribute modifications on product stock status and sales number made.

--- src/main/java/com/oak/bookyourshelf/controller/PaymentController.java
+++ src/main/java/com/oak/bookyourshelf/controller/PaymentController.java
@@ -1,45 +1,105 @@
 package com.oak.bookyourshelf.controller;
 
-import com.oak.bookyourshelf.model.CreditCard;
-import com.oak.bookyourshelf.model.Payment;
+import com.oak.bookyourshelf.model.*;
+import com.oak.bookyourshelf.service.AuthService;
+import com.oak.bookyourshelf.service.CartService;
 import com.oak.bookyourshelf.service.PaymentService;
+import com.oak.bookyourshelf.service.ProductService;
+import com.oak.bookyourshelf.service.product_details.ProductDetailsInformationService;
+import com.oak.bookyourshelf.service.profile.ProfileInformationService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.validation.Valid;
 import java.sql.Timestamp;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @Controller
 public class PaymentController {
 
     final PaymentService paymentService;
+    final ProfileInformationService profileInformationService;
+    final AuthService authService;
+    final CartService cartService;
+    final ProductDetailsInformationService productDetailsInformationService;
+    Order order = CartController.order;
 
-    public PaymentController(PaymentService paymentService) {
+    public PaymentController(PaymentService paymentService, @Qualifier("customUserDetailsService") AuthService authService,
+                             ProfileInformationService profileInformationService, CartService cartService,
+                             ProductDetailsInformationService productDetailsInformationService) {
         this.paymentService = paymentService;
+        this.profileInformationService = profileInformationService;
+        this.authService = authService;
+        this.cartService = cartService;
+        this.productDetailsInformationService = productDetailsInformationService;
+
     }
 
     @RequestMapping("/payment")
     public String showNewPaymentPage(Model model) {
         CreditCard creditCard = new CreditCard();
         model.addAttribute("creditCard", creditCard);
-
         return "payment";
     }
 
-    @RequestMapping(value = "/pay", method = RequestMethod.POST)
-    public String makePayment(@Valid @ModelAttribute CreditCard creditCard, BindingResult errors, Model model) {
-        if (!errors.hasErrors()) {
-            Payment payment = new Payment();
-            payment.setIssueDate(new Timestamp(System.currentTimeMillis()));
-            payment.setPayerId(1);
-            payment.setPaymentMethod(Payment.PaymentMethod.PAYMENT_METHOD_BANK_TRANSFER);
-            payment.setPaymentResult(Payment.PaymentResult.PAYMENT_RESULT_SUCCESS);
-            paymentService.save(payment);
+    @RequestMapping(value = "/payment", method = RequestMethod.POST)
+    public ResponseEntity<String> payment(@RequestParam String button, CreditCard creditCard) {
+        Payment payment = new Payment();
+        UserDetails userDetails = authService.getUserDetails();
+        User user = profileInformationService.getByEmail(userDetails.getUsername());
+
+
+        if (button.equals("ConfirmCreditCard")) {
+            System.out.println("In credit card");
+            payment.setPaymentMethod(Payment.PaymentMethod.PAYMENT_METHOD_CREDIT_CARD);
+            order.setPaymentOption(Order.PaymentOption.CREDIT_CARD);
+            payment(payment, user);
+            return ResponseEntity.ok("");
+
+        } else if (button.equals("PayPal")) {
+            payment.setPaymentMethod(Payment.PaymentMethod.PAYMENT_METHOD_PAYPAL);
+            order.setPaymentOption(Order.PaymentOption.PAYPAL);
+            payment(payment, user);
+            return ResponseEntity.ok("");
+
+        } else {
+            order.setPaymentStatus(Order.PaymentStatus.FAILED);
+            payment(payment, user);
+            return ResponseEntity.badRequest().body("Payment Failed");
         }
-        return ((errors.hasErrors()) ? "payment" : "thank-you");
+    }
+
+
+    public void payment(Payment payment, User user) {
+        payment.setIssueDate(new Timestamp(System.currentTimeMillis()));
+        payment.setPayerId(user.getUserId());
+        payment.setPaymentResult(Payment.PaymentResult.PAYMENT_RESULT_SUCCESS);
+        order.setOrderDate(new java.sql.Date(System.currentTimeMillis()));
+        order.setUserName(user.getName());
+        order.setUserId(user.getUserId());
+        order.setPaymentStatus(Order.PaymentStatus.COMPLETED);
+        productModification(user);
+        paymentService.save(payment);
+        cartService.save(order);
+        order = null;
+    }
+
+    public void productModification(User user) {
+        List<Integer> productIds = user.getShoppingCart().stream().map(Product::getProductId).collect(Collectors.toList());
+        for (Integer productID : productIds) {
+            Product product = productDetailsInformationService.get(productID);
+            product.increaseSalesNum();
+            product.getBuyerUserIds().add(user.getUserId());
+            product.setStock(product.getStock() - 1);
+            productDetailsInformationService.save(product);
+        }
+
     }
 }
