c334ea6b83282c7dd8891049280032afe485d68e
iseridilara <b21783561@cs.hacettepe.edu.tr>
2020-05-31 23:17:00 +0300

    pagination changed , globals class used in UserDetailsSearchController

--- src/main/java/com/oak/bookyourshelf/controller/user_details/UserDetailsSearchController.java
+++ src/main/java/com/oak/bookyourshelf/controller/user_details/UserDetailsSearchController.java
@@ -1,5 +1,6 @@
 package com.oak.bookyourshelf.controller.user_details;
 
+import com.oak.bookyourshelf.Globals;
 import com.oak.bookyourshelf.model.User;
 import com.oak.bookyourshelf.service.user_details.UserDetailsSearchService;
 import org.springframework.data.domain.Page;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -27,22 +29,14 @@ public class UserDetailsSearchController {
     @RequestMapping(value = "/user-details/search", method = RequestMethod.GET)
     public String tab(@RequestParam("id") int id, @RequestParam("size") Optional<Integer> size,
                       @RequestParam("page") Optional<Integer> page,  Model model) {
-        int currentPage = page.orElse(1);
-        int pageSize = size.orElse(2);
-        User user = userDetailsSearchService.get(id);
-        System.out.println("jbk");
-        Page<String> keyPage = userDetailsSearchService.findPaginated(PageRequest.of(currentPage - 1, pageSize), user);
-        model.addAttribute("keyPage", keyPage);
-        int totalPages = keyPage.getTotalPages();
-        if (totalPages > 0) {
-            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
-                    .boxed()
-                    .collect(Collectors.toList());
-            model.addAttribute("pageNumbers", pageNumbers);
-        }
-        model.addAttribute("user", user);
-        model.addAttribute("currentPage", currentPage);
 
+        User user = userDetailsSearchService.get(id);
+
+        List<String> keys = new ArrayList<>(user.getSearchHistory().keySet());
+        System.out.println("jbk");
+        Globals.getPageNumbers(page, size, (List) keys, model, "keyPage");
+
+        model.addAttribute("user", user);
         return "user_details/_search";
     }
 
