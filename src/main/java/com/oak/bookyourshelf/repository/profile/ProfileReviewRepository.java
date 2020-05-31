0e58928be4baf2979035c24fcd9fc5e0af09e303
fidansamet <fidansamet@hacettepe.edu.tr>
2020-05-31 15:09:58 +0300

    review listing performed in profile

--- src/main/java/com/oak/bookyourshelf/repository/profile/ProfileReviewRepository.java
+++ src/main/java/com/oak/bookyourshelf/repository/profile/ProfileReviewRepository.java
@@ -1,4 +1,11 @@
 package com.oak.bookyourshelf.repository.profile;
 
-public class ProfileReviewRepository {
+import com.oak.bookyourshelf.model.Review;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProfileReviewRepository extends CrudRepository<Review, Integer> {
+
+    @Query("SELECT r FROM Review r WHERE r.reviewId = ?1")
+    Review findReviewById(int id);
 }
