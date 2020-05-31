0f0498a142158d18ba30a87b773f53d58e93cf48
b21606478 <b21606478@cs.hacettepe.edu.tr>
2020-05-31 15:44:02 +0300

    Function to save order added

--- src/main/java/com/oak/bookyourshelf/repository/CartRepository.java
+++ src/main/java/com/oak/bookyourshelf/repository/CartRepository.java
@@ -1,8 +1,8 @@
 package com.oak.bookyourshelf.repository;
 
 import com.oak.bookyourshelf.model.Order;
-import com.oak.bookyourshelf.model.Product;
 import org.springframework.data.repository.CrudRepository;
+
 
 public interface CartRepository extends CrudRepository<Order, Integer> {
 
