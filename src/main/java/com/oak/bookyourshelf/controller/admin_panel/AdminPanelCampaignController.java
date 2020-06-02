0124be80a6245cc1df70164b4fa1f51409c3636b
iseridilara <b21783561@cs.hacettepe.edu.tr>
2020-06-02 13:31:00 +0300

    null subcategory check added to AdminPanelCampaignController

--- src/main/java/com/oak/bookyourshelf/controller/admin_panel/AdminPanelCampaignController.java
+++ src/main/java/com/oak/bookyourshelf/controller/admin_panel/AdminPanelCampaignController.java
@@ -41,18 +41,18 @@ public class AdminPanelCampaignController {
 
         return "admin_panel/_campaign";
     }
+
     @RequestMapping(value = "/admin-panel/campaign/category", method = RequestMethod.GET)
     @ResponseBody
     public List<Category> findAllCategories(@RequestParam String category) {
 
         if (category.equals("BOOK")) {
             System.out.println(adminPanelCategoryService.getAllByCategory("Book"));
-            return  (List<Category>) adminPanelCategoryService.getAllByCategory("Book");
-        }
-        else if(category.equals("E_BOOK"))
-            return  (List<Category>) adminPanelCategoryService.getAllByCategory("E-Book");
-        else if(category.equals("AUDIO_BOOK"))
-            return  (List<Category>) adminPanelCategoryService.getAllByCategory("Audio Book");
+            return (List<Category>) adminPanelCategoryService.getAllByCategory("Book");
+        } else if (category.equals("E_BOOK"))
+            return (List<Category>) adminPanelCategoryService.getAllByCategory("E-Book");
+        else if (category.equals("AUDIO_BOOK"))
+            return (List<Category>) adminPanelCategoryService.getAllByCategory("Audio Book");
         else return null;
 
     }
@@ -65,65 +65,59 @@ public class AdminPanelCampaignController {
 
     @RequestMapping(value = "/admin-panel/campaign", method = RequestMethod.POST)
     @ResponseBody
-    public ResponseEntity<String> saveCategory(@RequestParam String name, String category, String subctgry,String ptype ,Campaign campaign) {
+    public ResponseEntity<String> saveCategory(@RequestParam String name, String category, String subctgry, String ptype, Campaign campaign) {
         List<Campaign> sameType = adminPanelCampaignService.findAllByProductType(campaign.getProductType());
         List<Category> newCategoryList = new ArrayList<>();
-        List<Subcategory> newSubcategories = new ArrayList<Subcategory>();
-       if(ptype.equals("BOOK") || ptype.equals("E_BOOK") || ptype.equals("AUDIO_BOOK"))
-       {
-           Category newCategory = adminPanelCategoryService.getByName(category);
+        List<Subcategory> newSubcategories = new ArrayList<>();
+        if (ptype.equals("BOOK") || ptype.equals("E_BOOK") || ptype.equals("AUDIO_BOOK")) {
+            Category newCategory = adminPanelCategoryService.getByName(category);
 
-           newCategoryList.add(newCategory);
-           if(subctgry.equals(""))
-           {
-               for (Campaign i : sameType) {
-                   Category campaignCategory =i.getCategories().get(0);
-                   if(campaignCategory.getName().equals(category)) {
+            newCategoryList.add(newCategory);
+            if (subctgry.equals("")) {
+                for (Campaign i : sameType) {
+                    Category campaignCategory = i.getCategories().get(0);
+                    if (campaignCategory.getName().equals(category)) {
 
-                       return ResponseEntity.badRequest().body("There is a campaign in " + category +" category . Please change your selection");
+                        return ResponseEntity.badRequest().body("There is a campaign in " + category + " category . Please change your selection");
 
-                       }
-                   }
-               campaign.setCategories(newCategoryList);
-               }
-           else{
-               String[] subcategory = subctgry.split("-");
-               List<String> subcategories = Arrays.asList(subcategory);
-
-               for (Campaign i : sameType) {
-                   Category campaignCategory =i.getCategories().get(0);
-                   if(campaignCategory.getName().equals(category)) {
-                       for (String s : subcategories) {
-                           for(Subcategory sub : i.getSubcategories())
-                               if (sub.getName().equals(s)) {
-                                   return ResponseEntity.badRequest().body("There is a campaign in " + s +" subcategory . Please change your selection");
-                               }
-                       }
-                   }
-               }
+                    }
+                }
+                campaign.setCategories(newCategoryList);
+            } else {
+                String[] subcategory = subctgry.split("-");
+                List<String> subcategories = Arrays.asList(subcategory);
+                for (String s : subcategories) {
+                    Subcategory newSubcategory = adminPanelCategoryService.getSubcategory(newCategory, s);
+                    if (newCategory != null) {
+                        newSubcategories.add(newSubcategory);
+                    }
+                }
 
 
-               for (String s : subcategories) {
-                   Subcategory newSubcategory = adminPanelCategoryService.getSubcategory(newCategory,s);
-                   newSubcategories.add(newSubcategory);
-               }
+                for (Campaign i : sameType) {
+                    Category campaignCategory = i.getCategories().get(0);
+                    if (campaignCategory.getName().equals(category)) {
+                        for (Subcategory s : newSubcategories) {
+                            if (s.isInCampaign())
+                                return ResponseEntity.badRequest().body("There is a campaign in " + s.getName() + " subcategory . Please change your selection");
+                        }
+                    }
+                }
+                /*adminPanelCampaignService.setProductsRate(adminPanelCampaignService.createProductSet(newSubcategories), campaign.getRate());*/
 
-               campaign.setSubcategories(newSubcategories);
-               campaign.setCategories(newCategoryList);
-           }
+                campaign.setSubcategories(newSubcategories);
+                campaign.setCategories(newCategoryList);
+            }
 
-       }
+        } else {
+            if (sameType.size() > 0)
+                return ResponseEntity.badRequest().body("There is a campaign in " + ptype + " product type . Please change your selection");
 
-        else
-       {
-           if(sameType.size()>0)
-               return ResponseEntity.badRequest().body("There is a campaign in " + ptype +" product type . Please change your selection");
-
-            else{
-               campaign.setSubcategories(newSubcategories);
-               campaign.setCategories(newCategoryList);
-           }
-       }
+            else {
+                campaign.setSubcategories(newSubcategories);
+                campaign.setCategories(newCategoryList);
+            }
+        }
 
         adminPanelCampaignService.save(campaign);
 
