cb60360dc5567d2e85202dc03a3f408ae9740864
fidansamet <fidansamet@hacettepe.edu.tr>
2020-06-02 20:51:04 +0300

    filtering performed in user details review tab

--- src/main/java/com/oak/bookyourshelf/controller/user_details/UserDetailsReviewController.java
+++ src/main/java/com/oak/bookyourshelf/controller/user_details/UserDetailsReviewController.java
@@ -1,7 +1,7 @@
 package com.oak.bookyourshelf.controller.user_details;
 
 import com.oak.bookyourshelf.Globals;
-import com.oak.bookyourshelf.model.User;
+import com.oak.bookyourshelf.model.*;
 import com.oak.bookyourshelf.service.product_details.ProductDetailsInformationService;
 import com.oak.bookyourshelf.service.user_details.UserDetailsInformationService;
 import com.oak.bookyourshelf.service.user_details.UserDetailsReviewService;
@@ -10,7 +10,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Controller
 public class UserDetailsReviewController {
@@ -31,17 +33,20 @@ public class UserDetailsReviewController {
     public String tab(@RequestParam("id") int id,
                       @RequestParam("page") Optional<Integer> page,
                       @RequestParam("size") Optional<Integer> size,
-                      @RequestParam("sort") Optional<String> sort, Model model) {
+                      @RequestParam("sort") Optional<String> sort,
+                      @RequestParam("filter") Optional<String> filter, Model model) {
 
         User user = userDetailsInformationService.get(id);
         String currentSort = sort.orElse("date");
-        Globals.getPageNumbers(page, size, userDetailsReviewService.sortReviews(currentSort, user.getUserId()),
+        String currentFilter = filter.orElse("all");
+        Globals.getPageNumbers(page, size, filterReview(userDetailsReviewService.sortReviews(currentSort, user.getUserId()), currentFilter),
                 model, "reviewPage");
 
         model.addAttribute("user", user);
         model.addAttribute("reviews", user.getReviews());
         model.addAttribute("productService", productDetailsInformationService);
         model.addAttribute("sort", currentSort);
+        model.addAttribute("filter", currentFilter);
         return "user_details/_review";
     }
 
@@ -50,5 +55,22 @@ public class UserDetailsReviewController {
     public ResponseEntity<String> deleteReview(@PathVariable int id, @RequestParam int reviewId) {
         userDetailsReviewService.delete(reviewId);
         return ResponseEntity.ok("");
+    }
+
+    public List<Review> filterReview(List<Review> reviews, String rate) {
+        switch (rate) {
+            case "1":
+                return reviews.stream().filter(p -> p.getScoreOutOf5() == 1).collect(Collectors.toList());
+            case "2":
+                return reviews.stream().filter(p -> p.getScoreOutOf5() == 2).collect(Collectors.toList());
+            case "3":
+                return reviews.stream().filter(p -> p.getScoreOutOf5() == 3).collect(Collectors.toList());
+            case "4":
+                return reviews.stream().filter(p -> p.getScoreOutOf5() == 4).collect(Collectors.toList());
+            case "5":
+                return reviews.stream().filter(p -> p.getScoreOutOf5() == 5).collect(Collectors.toList());
+            default:
+                return reviews;
+        }
     }
 }
