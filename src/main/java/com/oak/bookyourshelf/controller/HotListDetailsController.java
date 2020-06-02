d340dc24d92d86a35166747dc50996fb62dff70e
iseridilara <b21783561@cs.hacettepe.edu.tr>
2020-06-02 23:24:23 +0300

    date and time check added to HotListDetailsController

--- src/main/java/com/oak/bookyourshelf/controller/HotListDetailsController.java
+++ src/main/java/com/oak/bookyourshelf/controller/HotListDetailsController.java
@@ -1,23 +1,28 @@
 package com.oak.bookyourshelf.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.oak.bookyourshelf.model.*;
 import com.oak.bookyourshelf.service.HotListDetailsService;
 import com.oak.bookyourshelf.service.admin_panel.AdminPanelCategoryService;
+import com.oak.bookyourshelf.service.admin_panel.AdminPanelHotListService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class HotListDetailsController {
 
     final HotListDetailsService hotListDetailsService;
     final AdminPanelCategoryService adminPanelCategoryService;
+    final AdminPanelHotListService adminPanelHotListService;
 
-    public HotListDetailsController(HotListDetailsService hotListDetailsService, AdminPanelCategoryService adminPanelCategoryService) {
+    public HotListDetailsController(HotListDetailsService hotListDetailsService, AdminPanelCategoryService adminPanelCategoryService, AdminPanelHotListService adminPanelHotListService) {
         this.hotListDetailsService = hotListDetailsService;
         this.adminPanelCategoryService = adminPanelCategoryService;
+        this.adminPanelHotListService = adminPanelHotListService;
     }
 
     @RequestMapping("/hotList-details/{id}")
@@ -40,6 +45,37 @@ public class HotListDetailsController {
         System.out.println("update");
         switch (button) {
             case "update_hotList":
+
+                String[] start = newHotList.getStartDate().split("/");
+                String[] end = newHotList.getEndDate().split("/");
+                String[] start_time = newHotList.getStartTime().split(":");
+                String[] end_time = newHotList.getEndTime().split(":");
+
+                List<String> startTime= Arrays.asList(start_time);
+                List<String> endTime = Arrays.asList(end_time);
+                List<String> startDate =Arrays.asList(start);
+                List<String> endDate = Arrays.asList(end);
+
+                if( !adminPanelHotListService.isDateValid(startDate))
+                    return ResponseEntity.badRequest().body("Start date is not valid");
+                if(!adminPanelHotListService.isDateValid(endDate))
+                    return ResponseEntity.badRequest().body("End date is not valid");
+                if(adminPanelHotListService.isDateValid(startDate) && adminPanelHotListService.isDateValid(endDate))
+                {
+                    if(!adminPanelHotListService.isDateCorrect(endDate,startDate))
+                        return ResponseEntity.badRequest().body("End date cannot be smaller than start date");
+                    else{
+                        if(!adminPanelHotListService.isTimeValid(startTime))
+                            return ResponseEntity.badRequest().body("Start time is not valid");
+                        if(!adminPanelHotListService.isTimeValid(endTime))
+                            return ResponseEntity.badRequest().body("End time is not valid");
+                        if(startDate.get(0).equals(endDate.get(0)) && startDate.get(1).equals(endDate.get(1)) && startDate.get(2).equals(endDate.get(2)))
+                        {
+                            if(!adminPanelHotListService.isTimeCorrect(startTime,endTime))
+                                return ResponseEntity.badRequest().body("If dates are the same , end time can not be smaller than start time");
+                        }
+                    }
+                }
 
                 hotList.setHotListType(newHotList.getHotListType());
                 hotList.setEndDate(newHotList.getEndDate());
