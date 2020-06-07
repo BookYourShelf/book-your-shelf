0ad15d420aae826dae9afb22ea14b6c28e2aa72d
iseridilara <b21783561@cs.hacettepe.edu.tr>
2020-06-08 00:00:46 +0300

    adding cart method fixed in home page controller

--- src/main/java/com/oak/bookyourshelf/controller/HomePageController.java
+++ src/main/java/com/oak/bookyourshelf/controller/HomePageController.java
@@ -2,10 +2,7 @@ package com.oak.bookyourshelf.controller;
 
 
 import com.oak.bookyourshelf.Globals;
-import com.oak.bookyourshelf.model.Image;
-import com.oak.bookyourshelf.model.Product;
-import com.oak.bookyourshelf.model.Review;
-import com.oak.bookyourshelf.model.User;
+import com.oak.bookyourshelf.model.*;
 import com.oak.bookyourshelf.service.AuthService;
 import com.oak.bookyourshelf.service.CompareProductsService;
 import com.oak.bookyourshelf.service.HomePageService;
@@ -20,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Controller
@@ -48,52 +46,30 @@ public class HomePageController {
         UserDetails userDetails = authService.getUserDetails();
         List<Product> ourPicksForYou = new ArrayList<>();
         if (userDetails != null) {
-            /*List<Image> bestSellerImages= createImageList(homePageService.createBestSellerList(0,8));*/
             model.addAttribute("bestSeller", homePageService.createBestSellerList(0, 8));
-            /*model.addAttribute("bestSellerImages", Globals.encodeAllImages(bestSellerImages));*/
 
-            /*List<Image> newReleasesImages= createImageList(homePageService.createNewProductsList(0,8));*/
             model.addAttribute("newProducts", homePageService.createNewProductsList(0, 8));
-            /*model.addAttribute("newReleasesImages",Globals.encodeAllImages(newReleasesImages));*/
 
-            /*List<Image> moreBestSellerImages = createImageList(homePageService.createBestSellerList(8,15));*/
             model.addAttribute("moreBestSeller", homePageService.createBestSellerList(8, 15));
-            /*model.addAttribute("moreBestSellerImages",Globals.encodeAllImages(moreBestSellerImages));*/
 
-            /*List<Image> moreNewReleasesImages = createImageList(homePageService.createNewProductsList(8,15));*/
             model.addAttribute("moreNewProducts", homePageService.createNewProductsList(8, 15));
-            /*model.addAttribute("moreNewReleasesImages",Globals.encodeAllImages(moreNewReleasesImages));*/
 
-            /*List<Image> ourPicksImages_1 = createImageList(homePageService.createBestSellerList(0,4));*/
             model.addAttribute("ourPicks_1", homePageService.createBestSellerList(0, 4));
-           /* model.addAttribute("ourPicksImages_1",Globals.encodeAllImages(ourPicksImages_1));*/
 
-            /*List<Image> ourPicksImages_2 = createImageList(homePageService.createBestSellerList(4,8));*/
             model.addAttribute("ourPicks_2", homePageService.createBestSellerList(4, 8));
-           /* model.addAttribute("ourPicksImages_2",Globals.encodeAllImages(ourPicksImages_2));*/
 
-            /*List<Image> ourPicksImages_3 = createImageList(homePageService.createBestSellerList(8,12));*/
             model.addAttribute("ourPicks_3", homePageService.createBestSellerList(8, 12));
-            /*model.addAttribute("ourPicksImages_3",Globals.encodeAllImages(ourPicksImages_3));*/
 
             model.addAttribute("user", profileInformationService.getByEmail(userDetails.getUsername()));
 
         } else {
-           /* List<Image> bestSellerImages= createImageList(homePageService.createBestSellerList(0,8));*/
             model.addAttribute("bestSeller", homePageService.createBestSellerList(0, 8));
-            /*model.addAttribute("bestSellerImages", Globals.encodeAllImages(bestSellerImages));*/
 
-           /* List<Image> newReleasesImages= createImageList(homePageService.createNewProductsList(0,8));*/
             model.addAttribute("newProducts", homePageService.createNewProductsList(0, 8));
-            /*model.addAttribute("newReleasesImages",Globals.encodeAllImages(newReleasesImages));*/
 
-           /* List<Image> moreBestSellerImages = createImageList(homePageService.createBestSellerList(8,15));*/
             model.addAttribute("moreBestSeller", homePageService.createBestSellerList(8, 15));
-            /*model.addAttribute("moreBestSellerImages",Globals.encodeAllImages(moreBestSellerImages));*/
 
-           /* List<Image> moreNewReleasesImages = createImageList(homePageService.createNewProductsList(8,15));*/
             model.addAttribute("moreNewProducts", homePageService.createNewProductsList(8, 15));
-           /* model.addAttribute("moreNewReleasesImages",Globals.encodeAllImages(moreNewReleasesImages));*/
 
             model.addAttribute("ourPicks_1", ourPicksForYou);
         }
@@ -114,30 +90,34 @@ public class HomePageController {
 
             switch (type) {
                 case "wish-list":
-                    if (!user.getWishList().contains(product)) {
+                    if (!containsProduct(user.getWishList(), product.getProductId())) {
                         user.addToWishList(product);
                         profileInformationService.save(user);
                     }
                     // already in wish list
                     return ResponseEntity.ok("");
 
+
                 case "cart":
-                    if (!user.getShoppingCart().contains(product)) {
+                    if (!containsCartItem(user.getShoppingCart(), product.getProductId())) {
                         if (product.getStock() > 0) {
-                            product.getProductQuantity().put(product.getProductId(), 1);
-                            user.addToCart(product);
-                            productDetailsInformationService.save(product);
+                            CartItem cartItem = new CartItem();
+                            cartItem.setProduct(product);
+                            cartItem.setQuantity(1);
+                            user.addToCart(cartItem);
                             profileInformationService.save(user);
                             return ResponseEntity.ok("");
+
                         } else {
                             return ResponseEntity.badRequest().body("There is no stock.");
                         }
+                    } else {
+                        // already in cart, quantity updated, save user
+                        profileInformationService.save(user);
+                        return ResponseEntity.ok("");
                     }
 
-                    product.getProductQuantity().put(product.getProductId(), product.getProductQuantity().get(product.getProductId()) + 1);
-                    productDetailsInformationService.save(product);
-                    // already in cart
-                    return ResponseEntity.ok("");
+
 
 
             }
@@ -149,15 +129,24 @@ public class HomePageController {
         return ResponseEntity.badRequest().body("An error occurred.");
     }
 
-    public List<Image> createImageList( List<Product> products)
-    {
-        List<Image> images = new ArrayList<>();
-        for(Product p:products)
-        {
-            images.add(p.getCoverImage());
-        }
-        return images;
 
+    public boolean containsCartItem(Set<CartItem> set, int productId) {
+        for (CartItem c: set) {
+            if(c.getProduct().getProductId() == productId) {
+                c.setQuantity(c.getQuantity() + 1);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean containsProduct(Set<Product> set, int productId) {
+        for (Product p: set) {
+            if(p.getProductId() == productId) {
+                return true;
+            }
+        }
+        return false;
     }
 
 
