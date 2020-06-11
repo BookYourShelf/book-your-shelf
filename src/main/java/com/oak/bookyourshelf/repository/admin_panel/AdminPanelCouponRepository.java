c7b377c27d064f16e3842d100601a2a19d38d223
b21606478 <b21606478@cs.hacettepe.edu.tr>
2020-06-11 17:31:42 +0300

    Admin Panel coupon repository added.

--- src/main/java/com/oak/bookyourshelf/repository/admin_panel/AdminPanelCouponRepository.java
+++ src/main/java/com/oak/bookyourshelf/repository/admin_panel/AdminPanelCouponRepository.java
@@ -2,11 +2,15 @@ package com.oak.bookyourshelf.repository.admin_panel;
 
 import com.oak.bookyourshelf.model.Coupon;
 import com.oak.bookyourshelf.model.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface AdminPanelCouponRepository extends CrudRepository<Coupon, Integer> {
+
+    @Query("SELECT t FROM Coupon t WHERE t.couponCode = ?1")
+    Coupon findByCouponCode(String couponCode);
 
     List<Coupon> findAllByOrderByCouponIdAsc();
 
