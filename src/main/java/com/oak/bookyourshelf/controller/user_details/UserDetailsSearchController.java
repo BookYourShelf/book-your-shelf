a3f272744e33c5e9c43b7d4b290f9e188758bee8
fidansamet <fidansamet@hacettepe.edu.tr>
2020-06-13 01:42:20 +0300

    user details search fixes

--- src/main/java/com/oak/bookyourshelf/controller/user_details/UserDetailsSearchController.java
+++ src/main/java/com/oak/bookyourshelf/controller/user_details/UserDetailsSearchController.java
@@ -3,18 +3,11 @@ package com.oak.bookyourshelf.controller.user_details;
 import com.oak.bookyourshelf.Globals;
 import com.oak.bookyourshelf.model.User;
 import com.oak.bookyourshelf.service.user_details.UserDetailsSearchService;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 @Controller
 public class UserDetailsSearchController {
@@ -25,19 +18,19 @@ public class UserDetailsSearchController {
         this.userDetailsSearchService = userDetailsSearchService;
     }
 
-
     @RequestMapping(value = "/user-details/search", method = RequestMethod.GET)
-    public String tab(@RequestParam("id") int id,@RequestParam("page") Optional<Integer> page, @RequestParam("size") Optional<Integer> size,
-                       @RequestParam("sort") Optional<String> sort, Model model) {
+    public String tab(@RequestParam("id") int id,
+                      @RequestParam("page") Optional<Integer> page,
+                      @RequestParam("size") Optional<Integer> size,
+                      @RequestParam("sort") Optional<String> sort, Model model) {
 
         User user = userDetailsSearchService.get(id);
         String currentSort = sort.orElse("Total-Search-desc");
 
-
-        Globals.getPageNumbers(page, size, userDetailsSearchService.sortSearchKey(currentSort,user), model, "keyPage");
+        Globals.getPageNumbers(page, size, userDetailsSearchService.sortSearchKey(currentSort, user), model, "keyPage");
         model.addAttribute("sort", currentSort);
         model.addAttribute("user", user);
-        model.addAttribute("searchValuesListEmpty",user.getSearchHistory().isEmpty());
+        model.addAttribute("searchValuesListEmpty", user.getSearchHistory().isEmpty());
         return "user_details/_search";
     }
 
