3389b4b2d6dc509df94d8c6d9abe6183e3acf3fb
oguzbakir <b21627007@cs.hacettepe.edu.tr>
2020-07-09 23:00:00 +0300

    Remove unnecessary tests

--- src/test/java/com/oak/bookyourshelf/ElectronicBookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.oak.bookyourshelf;
-
-import com.oak.bookyourshelf.model.ElectronicBook;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ElectronicBookRepository extends CrudRepository<ElectronicBook, Integer> {
-}
--- src/test/java/com/oak/bookyourshelf/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.oak.bookyourshelf;
-
-import com.oak.bookyourshelf.model.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-}
